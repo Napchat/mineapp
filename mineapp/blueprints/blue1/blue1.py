8527d1db03354daf404791a4e2d53fb90b9b5f06
Napchat <wozhendeaiwoa@gmail.com>
2017-06-28 17:11:21 +0800

    update

--- mineapp/blueprints/blue1/blue1.py
+++ mineapp/blueprints/blue1/blue1.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, g, redirect, url_for, session
 from jinja2 import TemplateNotFound
 from mineapp.forms import NameForm
 
@@ -6,13 +6,26 @@ blue1 = Blueprint('blue1', __name__, template_folder='templates', static_folder=
 
 @blue1.before_request
 def before_request():
-    pass
+    g.logged_in = False
+
+@blue1.route('/login', methods=['GET', 'POST'])
+def login():
+    try:
+        form = NameForm()
+        if form.validate_on_submit():
+            session['name'] = form.name.data
+            g.logged_in = True
+            return redirect(url_for('blue1.index'))
+        return render_template('blue1/login.html', form=form, name=session.get('name'))
+    except TemplateNotFound:
+        abort(404)
 
 @blue1.route('/')
 @blue1.route('/index')
 def index():
     try:
-        form = NameForm()
-        return render_template('blue1/index.html', form=form)
+        if not g.logged_in:
+            return redirect(url_for('blue1.login'))
+        return render_template('blue1/index.html')
     except TemplateNotFound:
         abort(404)
